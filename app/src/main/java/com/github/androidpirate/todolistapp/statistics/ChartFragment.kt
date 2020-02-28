7ad18b0037491762595b95a10f4ed73ae19dc81e
android_pirate <emrekalaycidesign@gmail.com>
2020-02-27 20:45:11 -0500

    Refactor - Implement a new method to change resource for chart image for day and night theme.

--- app/src/main/java/com/github/androidpirate/todolistapp/statistics/ChartFragment.kt
+++ app/src/main/java/com/github/androidpirate/todolistapp/statistics/ChartFragment.kt
@@ -1,11 +1,14 @@
 package com.github.androidpirate.todolistapp.statistics
 
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.preference.PreferenceManager
 
 import com.github.androidpirate.todolistapp.R
 
@@ -13,12 +16,36 @@ import com.github.androidpirate.todolistapp.R
  * A simple [Fragment] subclass.
  */
 class ChartFragment : Fragment() {
+    private lateinit var chartImage: ImageView
+    private lateinit var sharedPref: SharedPreferences
+    private var darkTheme = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        darkTheme = sharedPref.getBoolean(getString(R.string.pref_dark_theme_key), false)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        val view = inflater.inflate(R.layout.fragment_chart, container, false)
+        // Setup views
+        setupViews(view)
+        // Setup chart image resource
+        setChartImageRes()
+        return view
+    }
+
+    private fun setupViews(view: View) {
+        chartImage = view.findViewById(R.id.iv_chart)
+    }
+
+    private fun setChartImageRes() {
+        if(darkTheme) {
+            chartImage.setImageResource(R.drawable.ic_statistics_white)
+        }
     }
 
     companion object {
