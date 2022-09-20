6e67ea7f24720122e69f900961955b9bb6327aba
Emre Kalayci <emrekalaycidesign@gmail.com>
2022-09-20 14:35:07 -0400

    Refactor - Update package name and Hilt annotations.

--- app/src/main/java/com/example/taskmaster/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.taskmaster.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.taskmaster.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
