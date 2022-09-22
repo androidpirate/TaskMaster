aca59b122f6dd941af5ed9e2aa93e317a8753bad
Emre Kalayci <emrekalaycidesign@gmail.com>
2022-09-22 14:40:16 -0400

    New feat - Add CreateTaskFragment.

---
+++ app/src/main/java/com/example/taskmaster/ui/CreateTaskFragment.kt
@@ -0,0 +1,20 @@
+package com.example.taskmaster.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmaster.R
+
+
+class CreateTaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_task, container, false)
+    }
+}
